d181752e255ebd74f6a15913253d51f570ced09c
abhilashbag <abhilashbag03@gmail.com>
2024-02-25 14:05:40 +0530

    update

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function MyApp(){
+  return (
+    // <<div>
+    //   Custom App-moo
+    // </div>>
+    <App/>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
   // <React.StrictMode>
-    <App />
+    <MyApp/>
+
   // </React.StrictMode>,
 )
